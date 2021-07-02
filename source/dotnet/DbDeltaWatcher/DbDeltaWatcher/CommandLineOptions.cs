83ec5c64f26167ca643956d1eecb681858a3c34e
Stefan Hoffmann <stho32github@gmail.com>
2021-07-02 17:44:15 +0000

    public property should be written uppercase

--- source/dotnet/DbDeltaWatcher/DbDeltaWatcher/CommandLineOptions.cs
+++ source/dotnet/DbDeltaWatcher/DbDeltaWatcher/CommandLineOptions.cs
@@ -5,7 +5,7 @@ namespace DbDeltaWatcher
     public class CommandLineOptions
     {
         [Option(shortName: 'c', longName: "config", Required = false, HelpText = "Path to json based config file", Default = "BASEDIR")]
-        public string configFilePath { get; set; }
+        public string ConfigFilePath { get; set; }
         
         [Option(shortName: 'p', longName: "cspfile", Required = true, HelpText = "Path to connection string provider based on flat files")]
         public string FlatFileConnectionStringProviderFilePath { get; set; }
