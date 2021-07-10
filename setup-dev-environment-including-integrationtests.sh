3a5699ead4e6fdb6ba1c826face23b676802c1a8
Stefan Hoffmann <stho32github@gmail.com>
2021-07-10 09:17:08 +0200

    add script modification to activate integration tests

--- setup-dev-environment-including-integrationtests.sh
+++ setup-dev-environment-including-integrationtests.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
 echo "  - Installing mysql-server ..."
-#sudo apt-get install mysql-server -y
+sudo apt-get install mysql-server -y
 echo "--------------------------"
 echo " set password : Start123! "
 echo " thanks "
 echo "--------------------------"
 
-#sudo mysql_secure_installation
+sudo mysql_secure_installation
 
 echo "  - Installing SQL Server ..."
-#wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-#sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
-#sudo apt-get update
-#sudo apt-get install -y mssql-server
-#sudo /opt/mssql/bin/mssql-conf setup
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+sudo add-apt-repository "$(wget -qO- https://packages.microsoft.com/config/ubuntu/20.04/mssql-server-2019.list)"
+sudo apt-get update
+sudo apt-get install -y mssql-server
+sudo /opt/mssql/bin/mssql-conf setup
 
-#sudo apt install curl -y
-#curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
-#sudo apt-get update
-#sudo apt-get install mssql-tools unixodbc-dev -y
+sudo apt install curl -y
+curl https://packages.microsoft.com/config/ubuntu/20.04/prod.list | sudo tee /etc/apt/sources.list.d/msprod.list
+sudo apt-get update
+sudo apt-get install mssql-tools unixodbc-dev -y
 
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 
+cp ./IntegrationTestConfiguration.json_template ./source/dotnet/DbDeltaWatcher/IntegrationTestConfiguration.json
 
